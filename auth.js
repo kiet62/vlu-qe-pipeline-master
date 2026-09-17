bf7fa6da9898e337a6622abc6de805d804fce952
kiet62 <tankiet067@gmail.com>
2026-09-17 16:49:33 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ function login(username, password) {
         throw new Error("Password không được để trống");
     }
 
-    if (username === "admin" && password === "123") {
+    if (username === "admin" && password === "1234") {
         return {
             success: true,
             message: "Đăng nhập thành công",
